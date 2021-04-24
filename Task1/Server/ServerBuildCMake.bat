d16baada5d5f894285fa373bea3915e6264a007b
Anton <tosha37@inbox.ru>
2021-04-24 20:53:38 +0300

    Build script has been updated. Folder vendors/vcpkg creates in RtSoftSpringScool_21 folder, because it contains shared libraries.

--- Task1/Server/ServerBuildCMake.bat
+++ Task1/Server/ServerBuildCMake.bat
@@ -1,2 +1,5 @@
+set SOL_DIR=%~dp0
+set VCPKG_PATH=%~dp0..\..\vendors\vcpkg
+
 mkdir build
-cmake -B .\build -S . -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=J:/RtSoftSpringScool_21/Task1/vendors/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -B .\build -S . -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake
--- Task1/Task1_BuildCMake.bat
+++ Task1/Task1_BuildCMake.bat
@@ -1,5 +1,4 @@
 set SOL_DIR=%~dp0
-set VCPKG_PATH=%~dp0..\..\vendors\vcpkg
-
+set VCPKG_PATH=%~dp0..\vendors\vcpkg
 mkdir build
 cmake -B .\build -S . -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake
