ff3a0cc828b5d8856a550d63a1d976c071bfc855
KulikovAYU <tosha37@inbox.ru>
2022-01-21 01:00:03 +0300

    implemented generic wrapper for hash table

---
+++ Rtsoft_task/Task1/GenericForHashTable/GenericForHashTable/HashTableWrapper.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GenericForHashTable
+{
+    public class HashTableWrapper<TKey, TValue> : IDictionary<TKey, TValue>,
+        IEnumerable<KeyValuePair<TKey, TValue>>,
+        ICollection<KeyValuePair<TKey, TValue>>
+    {
+        public void Add(TKey key, TValue value) => items_.Add(key, value);
+
+        public void Add(KeyValuePair<TKey, TValue> keyValuePair) => items_.Add(keyValuePair.Key, keyValuePair.Value);
+       
+        public bool ContainsKey(TKey key) => items_.ContainsKey(key);
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (DictionaryEntry? item in items_)
+                yield return new KeyValuePair<TKey, TValue>((TKey)item.Value.Key, (TValue)item.Value.Value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (ContainsKey(key)) {
+                items_.Remove(key);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            value = default(TValue);
+
+            if (ContainsKey(key)) 
+            {
+                value = (TValue)items_[key];
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            items_.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return items_.Contains(item.Key);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            items_.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            if (Contains(item)) {
+                items_.Remove((TKey)item.Key);
+                return true;
+            }
+
+           return false;
+        }
+
+        public ICollection<TKey> Keys {
+
+            get {
+                var buff = new List<TKey>();
+                foreach (var item in items_.Keys)
+                    buff.Add((TKey)item);
+
+                return buff;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                var buff = new List<TValue>();
+                foreach (var item in items_.Values)
+                    buff.Add((TValue)item);
+
+                return buff;
+            }
+        }
+
+        public int Count => items_.Count;
+
+        public bool IsReadOnly => items_.IsReadOnly;
+
+        public TValue this[TKey key] { get => (TValue)items_[key]; set => items_[key] = value; }
+        
+        private Hashtable items_ = new Hashtable();
+    }
+}
---
+++ Rtsoft_task/Task1/GenericForHashTable/Tests/UnitTest.cs
@@ -0,0 +1,90 @@
+using GenericForHashTable;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+    class TestDataGenerator
+    {
+    
+        public static HashTableWrapper<int, string> GetTestData()
+        {
+            HashTableWrapper<int, string> table_ = new HashTableWrapper<int, string>();
+            table_.Add(0, "Нуль");
+            table_.Add(1, "Один");
+            table_.Add(2, "Два");
+            table_.Add(3, "Три");
+
+            return table_;
+        }
+    }
+
+
+    public class ValidityTest
+    {
+        [Fact]
+        public void CheckCount()
+        {
+            var table_ = TestDataGenerator.GetTestData();
+            Assert.True(table_.Count == 4, $"Counting was failed");
+        }
+
+        [Fact]
+        public void AddOneElement()
+        {
+            var table_ = TestDataGenerator.GetTestData();
+            int key = 4;
+            string val = "Четыре";
+            table_.Add(key, "Четыре");
+            Assert.True(table_[key] == val, $"Failed add key = {key} and value = {val} to container");
+        }
+
+        [Fact]
+        public void LookThrow()
+        {
+            var table_ = TestDataGenerator.GetTestData();
+            List<string> vars = new List<string> { "Нуль", "Один", "Два", "Три" };
+           
+            for (int i = 0; i < table_.Count; ++i)
+                Assert.True(table_[i] == vars[i], "Counting was failed");
+        }
+
+        [Fact]
+        public void CheckEnumerator()
+        {
+            var table_ = TestDataGenerator.GetTestData();
+
+            List<int> keys = new List<int> { 0, 1, 2, 3 };
+            List<string> values = new List<string> { "Нуль", "Один", "Два", "Три" };
+            keys.Reverse();
+            values.Reverse();
+
+            int keyIndex = 0;
+            int valuesIndex = 0;
+            foreach (var item in table_)
+            {
+                Assert.True(item.Key == keys[keyIndex++] &&
+                     item.Value == values[valuesIndex++], "Enumerating was failed");
+            }
+        }
+
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(10, false)]
+        public void ContainsKey(int key, bool expectedResult)
+        {
+            var table_ = TestDataGenerator.GetTestData();
+            Assert.True(table_.ContainsKey(key) == expectedResult, $"Container doesn't contain key = {key}");
+        }
+
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(20, false)]
+        public void RemoveOneElementByKey(int key, bool expectedResult)
+        {
+            var table_ = TestDataGenerator.GetTestData();
+            Assert.True(table_.Remove(key) == expectedResult, $"Failed to remove element by key = {key}");
+        }
+
+    }
+}
